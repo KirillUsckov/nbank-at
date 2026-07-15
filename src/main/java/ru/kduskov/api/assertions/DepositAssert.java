b639b3e1af30cf452be198d70a0abb4b458a8d48
Kirill <kir.usckov@gmail.com>
2026-07-15 15:30:37 +0300

    test changes for trigger

--- src/main/java/ru/kduskov/api/assertions/DepositAssert.java
+++ src/main/java/ru/kduskov/api/assertions/DepositAssert.java
@@ -6,7 +6,6 @@ import ru.kduskov.api.models.body.response.accounts.deposit.DepositResponseBody;
 import ru.kduskov.api.models.body.response.general.AccountResponseBody;
 
 public class DepositAssert extends BaseAssert<DepositAssert, DepositResponseBody> {
-
     public DepositAssert(DepositResponseBody actual, SoftAssertions softly) {
         super(actual, DepositAssert.class, softly);
     }
