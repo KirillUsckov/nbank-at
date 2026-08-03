f0f0284ca23486569c164bd802201ac0e8753678
Kirill <kir.usckov@gmail.com>
2026-08-03 21:32:21 +0300

    upd url for ui

--- infra/kube/port-forward.sh
+++ infra/kube/port-forward.sh
@@ -2,3 +2,4 @@ kubectl port-forward svc/frontend 3000:80 2>&1 &
 kubectl port-forward svc/backend 4111:4111 2>&1 &
 kubectl port-forward svc/selenoid 4444:4444 2>&1 &
 kubectl port-forward svc/selenoid-ui 8080:2508 2>&1 &
+kubectl port-forward svc/postgres 5432:5432 2>&1 &
